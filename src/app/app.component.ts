0bb4d11168e78ab5a95f1b06253e83c03ed0b24c
Aleksey Krasnov <neky@list.ru>
2021-06-14 00:52:53 +0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'background-test';
+  public form: FormGroup;
+  unsubcribe: any
+
+  public fields: any[] = [
+    {
+      type: 'text',
+      name: 'firstName',
+      label: 'Имя',
+      value: '',
+      required: true,
+    },
+    {
+      type: 'text',
+      name: 'lastName',
+      label: 'Фамилия',
+      value: '',
+      required: true,
+    },
+    {
+      type: 'text',
+      name: 'email',
+      label: 'E-mail',
+      value: '',
+      required: false,
+    },
+
+    {
+      type: 'dropdown',
+      name: 'country',
+      label: 'Country',
+      value: 'in',
+      required: true,
+      options: [
+        { key: 'in', label: 'India' },
+        { key: 'us', label: 'USA' }
+      ]
+    },
+    {
+      type: 'checkbox',
+      name: 'hobby',
+      label: 'Hobby',
+      required: true,
+      options: [
+        { key: 'f', label: 'Fishing' },
+        { key: 'c', label: 'Cooking' }
+      ]
+    }
+  ];
+
+  constructor() {
+    this.form = new FormGroup({
+      fields: new FormControl(JSON.stringify(this.fields))
+    })
+    this.unsubcribe = this.form.valueChanges.subscribe((update) => {
+      console.log(update);
+      this.fields = JSON.parse(update.fields);
+    });
+  }
+
+  onUpload(e:any) {
+    console.log(e);
+
+  }
+
+  getFields() {
+    return this.fields;
+  }
+
+  ngDistroy() {
+    this.unsubcribe();
+  }
 }
